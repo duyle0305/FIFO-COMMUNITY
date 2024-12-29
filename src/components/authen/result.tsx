b845add3cfc47f33d508d83e343690112f3a068d
DuyLe <duyle030500@gmail.com>
2024-12-29 11:23:21 +0700

    [update] routes

--- src/components/authen/result.tsx
+++ src/components/authen/result.tsx
@@ -1,54 +1,52 @@
-import { Flex, Typography } from "antd";
-import { FC } from "react";
-import { Link } from "react-router-dom";
-import BaseButton from "../core/button";
-import { SuccessfulIcon } from "@/utils/asset";
-import { css } from "@emotion/react";
-import AuthFormTitle from "./form-title";
-import AuthFormDescription from "./form-desciption";
+import type { FC } from 'react';
+
+import { css } from '@emotion/react';
+import { Flex, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+
+import { SuccessfulIcon } from '@/utils/asset';
+
+import BaseButton from '../core/button';
+import AuthFormDescription from './form-desciption';
+import AuthFormTitle from './form-title';
 
 interface AuthResultPageProps {
-    title?: string
-    description?: string
-    btnNavigateTo?: string
-    btnText?: string
-    icon?: string
+    title?: string;
+    description?: string;
+    btnNavigateTo?: string;
+    btnText?: string;
+    icon?: string;
 }
+
 const AuthResultPage: FC<AuthResultPageProps> = ({ title, description, btnNavigateTo, btnText, icon }) => {
-    return <div css={styles}>
-        <Flex vertical align="center" style={{ width: "100%" }}>
-            {icon && (
-                <img className="result-success-icon" src={icon} alt="Success Icon" />
-            )}
+    return (
+        <div css={styles}>
+            <Flex vertical align="center" style={{ width: '100%' }}>
+                {icon && <img className="result-success-icon" src={icon} alt="Success Icon" />}
 
-            {title && (
-                <div className="result-title">
-                    <AuthFormTitle title={title} />
-                </div>
-            )}
+                {title && (
+                    <div className="result-title">
+                        <AuthFormTitle title={title} />
+                    </div>
+                )}
 
-            {description && (
-                <div className="result-description">
-                    <AuthFormDescription description={description} />
-                </div>
-            )}
+                {description && (
+                    <div className="result-description">
+                        <AuthFormDescription description={description} />
+                    </div>
+                )}
 
-            {btnNavigateTo && btnText && (
-                <Link className="result-btn" to={btnNavigateTo}>
-                    <BaseButton
-                        size="large"
-                        variant="outlined"
-                        shape="round"
-                        type="primary"
-                    >
-                        {btnText}
-                    </BaseButton>
-                </Link>
-            )}
-        </Flex>
-    </div>
-}
-
+                {btnNavigateTo && btnText && (
+                    <Link className="result-btn" to={btnNavigateTo}>
+                        <BaseButton size="large" variant="outlined" shape="round" type="primary">
+                            {btnText}
+                        </BaseButton>
+                    </Link>
+                )}
+            </Flex>
+        </div>
+    );
+};
 
 const styles = css(`
     .result-success-icon {
@@ -65,5 +63,6 @@ const styles = css(`
         width: 100%;
     }
     
-`)
-export default AuthResultPage
+`);
+
+export default AuthResultPage;
--- src/routes/private-route.tsx
+++ src/routes/private-route.tsx
@@ -1,30 +1,28 @@
 import type { FC } from 'react';
 import type { RouteProps } from 'react-router';
 
-import BaseButton from '@/components/core/button';
 import { Result } from 'antd';
 import { useLocation } from 'react-router';
 import { useNavigate } from 'react-router-dom';
 
-const PrivateRoute: FC<RouteProps> = props => {
-  const navigate = useNavigate();
-  const location = useLocation();
+import BaseButton from '@/components/core/button';
 
-  return (
-    <Result
-      status="403"
-      title="403"
-      subTitle="Forbidden"
-      extra={
-        <BaseButton
-          type="primary"
-          onClick={() => navigate("/")}
-        >
-          Go to the Home Page
-        </BaseButton>
-      }
-    />
-  );
+const PrivateRoute: FC<RouteProps> = props => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    return (
+        <Result
+            status="403"
+            title="403"
+            subTitle="Forbidden"
+            extra={
+                <BaseButton type="primary" onClick={() => navigate('/')}>
+                    Go to the Home Page
+                </BaseButton>
+            }
+        />
+    );
 };
 
 export default PrivateRoute;
